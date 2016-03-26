a980c40210373ecd1f9a7b74fead731ece2fc48e
Andrew Timson <andrew@timson.org>
2016-03-25 23:31:08 -0400

    Move Bower components outside of source control.

--- src/Tracker/gulpfile.js
+++ src/Tracker/gulpfile.js
@@ -5,7 +5,12 @@ var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    filter = require("gulp-filter"),
+    mainBowerFiles = require("gulp-main-bower-files"),
+    print = require("gulp-print"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify"),
+    merge = require("merge-stream");
 
 var paths = {
     webroot: "./wwwroot/"
@@ -27,6 +32,61 @@ gulp.task("clean:css", function (cb) {
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
+
+var mainBowerFilesOverrides = {
+    overrides: {
+        // Bootstrap's bower.json only specifies the JavaScript.
+        bootstrap: {
+            main: [
+                './dist/js/bootstrap.js',
+                './dist/css/*.css',
+                './dist/fonts/*.*'
+            ]
+        }
+    }
+};
+
+gulp.task('main-bower-files', function () {
+    // This will create minified versions of all the library CSS/JS files. It
+    // will not (yet) combine them. It will not persist any pre-minified files.
+
+    var minifiedCSS = gulp.src('./bower.json')
+        .pipe(mainBowerFiles(['**/*.css', '!**/*.min.css'], mainBowerFilesOverrides))
+        .pipe(print(function (path) {
+            return "MinifyCSS: " + path;
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(print(function (path) {
+            return "MiniCSS: " + path;
+        }))
+        .pipe(cssmin());
+
+    var minifiedJS = gulp.src('./bower.json')
+        .pipe(mainBowerFiles(['**/*.js', '!**/*.min.js'], mainBowerFilesOverrides))
+        .pipe(print(function (path) {
+            return "MinifyJS: " + path;
+        }))
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+        .pipe(print(function (path) {
+            return "MiniJS: " + path;
+        }))
+        .pipe(uglify());
+
+    var raw = gulp.src('./bower.json')
+        .pipe(mainBowerFiles(['**/*.*', '!**/*.min.css', '!**/*.min.js'], mainBowerFilesOverrides))
+        .pipe(print(function (path) {
+            return "RAW: " + path;
+        }))
+        .pipe(gulp.dest(paths.webroot + 'lib/'));
+
+    return merge(minifiedCSS, minifiedJS, raw)
+        .pipe(gulp.dest(paths.webroot + 'lib/'))
+        .pipe(print());
+});
 
 gulp.task("min:js", function () {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
